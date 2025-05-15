bff412018b2878a68a38a1f8fd55b70e842067df
thepKz <thepmtse181568@fpt.edu.vn>
2025-05-16 00:03:39 +0700

    Refactor: Change BrowserRouter to HashRouter

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './redux/store';
 import './styles/index.css';
@@ -9,9 +9,9 @@ import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 ); 
